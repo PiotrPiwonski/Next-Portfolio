98ccd134d44e5c1dfb7b3ec04f298692622273d4
PiotrPiwonski <49531459+PiotrPiwonski@users.noreply.github.com>
2024-01-26 11:44:31 +0100

    add HomeComponen and first styles

---
+++ src/app/components/HomeComponent/HomeComponent.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function HomeComponent() {
+  return (
+    <React.Fragment>
+        <div id='home'
+        style={{
+            backgroundImage: "linear-gradient(62deg, #EEF7FB 0 50%, #A8AFDE 0% 100%)",
+            minHeight: "500px",
+            maxHeight: "1200px",
+            height: "100%",
+            width: "100%"
+        }}
+        >
+            
+        </div>
+    </React.Fragment>
+  )
+}
